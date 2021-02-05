2d4df29e9dba5c3e20caebaf8a57a0eae90e8074
mouna07 <mounamc261@gmail.com>
2021-02-05 19:07:09 +0530

    ADDED: Ability to add and display multiple contacts

--- AddressBook.java
+++ AddressBook.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class AddressBook {
+    // Constants
+    public static final int MAX_CONTACTS_LIMIT = 10;
 
+    // Public Variables
     public String firstName, lastName, address, city, state;
-    public String zip, phoneNumber;
+    public int zip, phoneNumber;
 
-    public AddressBook(String firstName, String lastName, String address, String city, String state, String zip,
-            String phoneNumber) {
+    public AddressBook(String firstName, String lastName, String address, String city, String state, int zip,
+            int phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -20,12 +25,35 @@ public class AddressBook {
         System.out.println("City: " + city);
         System.out.println("State: " + state);
         System.out.println("Zip Code: " + zip);
-        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Phone Number: " + phoneNumber + "\n");
     }
 
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        int choice = 2;
         System.out.println("Welcome to AddressBook!\n");
-        AddressBook addressBook = new AddressBook(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
-        addressBook.displayContact();
+        AddressBook addressBook[] = new AddressBook[MAX_CONTACTS_LIMIT];
+        while (i < MAX_CONTACTS_LIMIT) {
+            System.out.println("Choice: 1] Add Contact \t2] Display Contacts \t3] Exit");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println(
+                            "Please Write in Format.\nFirst_Name Last_Name Adderss City State Zip Phone_Number");
+                    addressBook[i] = new AddressBook(scanner.next(), scanner.next(), scanner.next(), scanner.next(),
+                            scanner.next(), scanner.nextInt(), scanner.nextInt());
+                    i++;
+                    break;
+                case 2:
+                    for (int j = 0; j < i; j++) {
+                        addressBook[j].displayContact();
+                    }
+                    break;
+                default:
+                    System.exit(0);
+                    break;
+            }
+        }
     }
 }
